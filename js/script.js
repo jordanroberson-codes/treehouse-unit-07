5c20988e97d9a7c04c53704fa0f3d7d46104c1f9
Jordan Roberson <jordanrobersonnp@gmail.com>
2021-12-11 14:11:43 -0500

    updated job role section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("test");
+// Adding a focus state to the name input by obtaining the element ID.
+let nameInput = document.getElementById("name");
+nameInput.focus();
+
+//job role programming
+let jobRole = document.getElementById("title"); //variable for job role by getting element ID of title.
+let otherRole = document.getElementById("other-job-role"); //variable for other job role by getting element id of other-job-role
+
+otherRole.style.display = "none"; //hiding other job role upon loading
+
+// adding event listener to the jobRole select menu that when 'other' is selected the other job role input text box appears otherwise it stays hidden.
+jobRole.addEventListener("change", (event) => {
+  if (event.target.value === "other") {
+    otherRole.style.display = "block";
+  } else {
+    otherRole.style.display = "none";
+  }
+});
+
+console.log();
