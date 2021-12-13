dbb4ab65b7cb5f08f2f9007ea6bc88762cf91eb8
Jordan Roberson <jordanrobersonnp@gmail.com>
2021-12-13 05:19:33 -0500

    tshirt section added

--- js/script.js
+++ js/script.js
@@ -18,11 +18,23 @@ jobRole.addEventListener("change", (event) => {
 });
 
 //TShirt Section
-let designTshirt = document.getElementById("design");
-let colorShirt = document.getElementById("color");
-let colorOptions = colorShirt.children;
-colorShirt.disabled = true;
+let designTshirt = document.getElementById("design"); //variable selecting design ID
+let colorShirt = document.getElementById("color"); //variable selecting color ID
+let colorOptions = colorShirt.children; //variable selecting the children of colorShirt variable
+colorShirt.disabled = true; //setting colorShirt attribute to hidden
+//adding an event listener to designTshirt to show available colors only in specific design shirt options.
 designTshirt.addEventListener("change", (event) => {
   colorShirt.disabled = false;
+  for (i = 0; i < colorShirt.length; i++) {
+    const colorValue = event.target.value;
+    const dataTheme = colorOptions[i].getAttribute("data-theme");
+    if (colorValue === dataTheme) {
+      colorOptions[i].hidden = false;
+      colorOptions[i].setAttribute("selected", true);
+    } else if (colorValue !== dataTheme) {
+      colorOptions[i].hidden = true;
+      colorOptions[i].removeAttribute("selected");
+    }
+  }
 });
-console.log(colorOptions);
+// Register for Activities Section
